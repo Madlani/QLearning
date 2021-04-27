28af7c938e00ffc746c6e44eee1246180618d472
marwane <marwane@udel.edu>
2021-04-27 18:48:56 -0400

    added code to handle edge cases

--- qlearn.py
+++ qlearn.py
@@ -32,15 +32,15 @@ for i in range (0,16,1):
     boardArray.append(Square('O',0,-1,i))
 
 
-print("Initialized all to -1 = ")
-for i in range (0,16,1):
-    print(boardArray[i].type, "+ ",boardArray[i].reward)
+#print("Initialized all to -1 = ")
+#for i in range (0,16,1):
+#    print(boardArray[i].type, "+ ",boardArray[i].reward)
 
 inputString = input ("Enter input-")
 inputAsArray = inputString.split(" ")
 inputAsArrayLen = len(inputAsArray)
 
-print(inputAsArray)
+#print(inputAsArray)
 
 goalState1Loc = int(inputAsArray[0])
 goalState2Loc = int(inputAsArray[1])
@@ -48,7 +48,7 @@ forbiddenStateLoc = int(inputAsArray[2])
 wallStateLoc = int(inputAsArray[3])
 outputFormat = inputAsArray[4]
 
-if (inputAsArrayLen == 5):
+if (inputAsArrayLen == 6):
     printQValuesIndex = inputAsArray[5]
 
 startLoc = 2
@@ -68,9 +68,9 @@ boardArray[forbiddenStateLoc].reward = -100
 boardArray[wallStateLoc].type = 'W'
 
 
-print("Set all starting states")
-for i in range (0,16,1):
-    print(boardArray[i].type, "+ ",boardArray[i].qVal)
+# print("Set all starting states")
+# for i in range (0,16,1):
+#     print(boardArray[i].type, "+ ",boardArray[i].qVal)
 
 
 
@@ -82,13 +82,70 @@ def isTerminalState (square):
     else:
         return False
 
+rightBorder = {16,12,8,4}
+leftBorder = {13,9,4,1}
+upBorder = {13,14,15,16}
+downBorder = {1,2,3,4}
+#we need to limit certain squares,
+# #can't go up for example on top row,
+# 
+upVal = None
+downVal = None
+eftVal = None
+    rightVal = None
 def chooseNextState(square):
-    rightVal = boardArray[square.index+1].qVal
-    leftVal = boardArray[square.index-1].qVal
-    upVal = boardArray[square.index+4].qVal
-    downVal = boardArray[square.index-4].qVal
-    maxVal = max(rightVal, leftVal, upVal, downVal)
+
+    if square.index in rightBorder:
+        if square.index in upBorder: #Square 16
+            downVal = boardArray[square.index-4].qVal
+            leftVal = boardArray[square.index-1].qVal
+        elif square.index in downBorder:#Square 4
+            upVal = boardArray[square.index+4].qVal    
+            leftVal = boardArray[square.index-1].qVal
+        else:#Any square right NOT 16/4
+            upVal = boardArray[square.index+4].qVal    
+            downVal = boardArray[square.index-4].qVal
+            leftVal = boardArray[square.index-1].qVal
+
+    elif square.index in leftBorder:
+        if square.index in upBorder: #Square 13
+            downVal = boardArray[square.index-4].qVal
+            rightVal = boardArray[square.index+1].qVal
+        elif square.index in downBorder: #Square 1
+            upVal = boardArray[square.index+4].qVal    
+            rightVal = boardArray[square.index+1].qVal
+        else:#Any square right NOT 13/1
+            upVal = boardArray[square.index+4].qVal    
+            downVal = boardArray[square.index-4].qVal
+            rightVal = boardArray[square.index+1].qVal
+
+    elif square.index in upBorder:
+        if square.index in rightBorder: #Square 16
+            downVal = boardArray[square.index-4].qVal
+            leftVal = boardArray[square.index-1].qVal
+        elif square.index in leftBorder:#Square 13
+            downVal = boardArray[square.index-4].qVal
+            rightVal = boardArray[square.index+1].qVal
+        else:#Any square right NOT 16/13
+            downVal = boardArray[square.index-4].qVal
+            rightVal = boardArray[square.index+1].qVal
+            leftVal = boardArray[square.index-1].qVal
+
+        
+    elif square.index in downBorder:
+        if square.index in rightBorder: #Square 4
+            upVal = boardArray[square.index+4].qVal
+            leftVal = boardArray[square.index-1].qVal
+        elif square.index in leftBorder:#Square 1
+            upVal = boardArray[square.index+4].qVal
+            rightVal = boardArray[square.index+1].qVal
+        else:#Any square right NOT 4/1
+            upVal = boardArray[square.index+4].qVal
+            rightVal = boardArray[square.index+1].qVal
+            leftVal = boardArray[square.index-1].qVal
     
+    
+    maxVal = max(rightVal, leftVal, upVal, downVal)
     bestState = None 
 
     if maxVal==rightVal:
@@ -121,11 +178,11 @@ def chooseNextState(square):
 
 
 def updateQVal (state):
-    print("current q val = ",state.qVal)
+    #print("current q val = ",state.qVal)
     newQVal = chooseNextState (state).qVal
     tempQval = ((1-learnRate) * state.qVal) + learnRate[state.reward + discountRate *newQVal]
     state.qVal = tempQval
-    print("new q val = ",state.qVal)
+   # print("new q val = ",state.qVal)
 
 
 
